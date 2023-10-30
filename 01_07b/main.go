554d1067da435fc7f7f4c7e4dcdba4e3e0a2ca36
RaghurajTarikere <raghuraj.tp1992@gmail.com>
2023-10-30 21:22:08 +0530

    Solution to 01_07b

--- 01_07b/main.go
+++ 01_07b/main.go
@@ -8,11 +8,62 @@ import (
 // isBalanced returns whether the given expression
 // has balanced brackets.
 func isBalanced(expr string) bool {
-	panic("NOT IMPLEMENTED")
+	var myStack stack
+
+	for _, bracket := range expr {
+		if isOpeningBracketType(bracket) {
+			myStack.push(bracket)
+		} else if isClosingBracketType(bracket) {
+			curBracket := myStack.pop()
+			if curBracket != getMatchingBracketType(bracket) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func isOpeningBracketType(e rune) bool {
+	return e == '{' || e == '[' || e == '('
+}
+
+func isClosingBracketType(e rune) bool {
+	return e == '}' || e == ']' || e == ')'
+}
+
+func getMatchingBracketType(e rune) rune {
+	switch e {
+	case '}':
+		return '{'
+	case ']':
+		return '['
+	case ')':
+		return '('
+	default:
+		log.Fatal("Invalid bracket type")
+	}
+	return ' '
+}
+
+type stack struct {
+	elems []rune
+}
+
+func (s *stack) push(e rune) {
+	s.elems = append(s.elems, e)
+}
+
+func (s *stack) pop() rune {
+	if len(s.elems) == 0 {
+		log.Fatal("No elems in stack to pop")
+	}
+	elem := s.elems[len(s.elems)-1]
+	s.elems = s.elems[:len(s.elems)-1]
+	return elem
 }
 
 // printResult prints whether the expression is balanced.
-func printResult(expr string, balanced bool){ 
+func printResult(expr string, balanced bool) {
 	if balanced {
 		log.Printf("%s is balanced.\n", expr)
 		return
